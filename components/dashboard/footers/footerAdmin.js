f098117cbfbec59f8de48318ff07a7aaa97c6edb
Shazaniyu55 <shazaniyu@gmail.com>
2024-05-11 21:26:24 +0100

    niyu new

--- components/dashboard/footers/footerAdmin.js
+++ components/dashboard/footers/footerAdmin.js
@@ -6,8 +6,7 @@ import CardLineChart from "../cards/cardLineChart";
 export default function FooterAdmin() {
   return (
     <>
-      <CardSocialTraffic/>
-
+      <CardBarChart/>
 
 {/* <div className="flex flex-wrap">
         <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
