8065fe5fb1a20cb4d41cb5ec1692596a956d2d96
Shazaniyu55 <shazaniyu@gmail.com>
2024-05-14 17:03:41 +0100

    john2@

--- pages/contact.js
+++ pages/contact.js
@@ -30,6 +30,17 @@ function Contact(){
                 </div>
                 
             </div>
+
+            {/* next container */}
+
+            <div className='md:flex justify-end'>
+            <div className="md:container bg-blue-200 m-10 p-4">
+            <h1 className='text-4xl md:text-8xl font-bold text-blue-700'>Do you have a general concern?</h1>
+               <p className="text-blue-800 mt-4">Then contact us quickly and easily using the online form.</p>
+            </div>
+            </div>
+
+           
         </div>
     );
 }
