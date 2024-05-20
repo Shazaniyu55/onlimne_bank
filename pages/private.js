61499897f316d2aa4e7720910951f6257c143d8d
Shazaniyu55 <shazaniyu@gmail.com>
2024-05-20 11:45:22 +0100

    amaka

--- pages/private.js
+++ pages/private.js
@@ -41,21 +41,37 @@ function Private(){
 
 
                 </div>
-                            </div>
+              </div>
 
 
         {/* container1 */}
 
-        <div className='md:flex justify-end mt-8'>
-            <div className="md:container md:w-full bg-blue-100 md:m-10 p-4">
+<div className='md:flex justify-end mt-8'>
+<div className="md:container md:w-full bg-blue-100 md:m-10 p-4">
 
+<div className="md:flex items-center space-x-4 text-blue-700">
+<Image src="/svg/family.svg" className="-translate-y-34" alt='chat-bubbles' width={450} height={450}/>
+<div className="flex-1">
+<h1 className='text-4xl md:text-4xl font-bold text-blue-700'>What interests you today?</h1>
+<div className="grid grid-cols-2 gap-4 mt-8">
 
-            <div className="grid md:grid-cols-2">
-            <Image src="/svg/family.svg" className="-translate-y-34" alt='chat-bubbles' width={450} height={450}/>
-            <h1 className='text-4xl md:text-4xl font-bold text-blue-700'>What interests you today?</h1>
+                  <p className="font-bold text-2xl">Accounts and Cards</p>
+                  <p className="font-bold text-2xl">Invest sustainably</p>
+                  <p className="font-bold text-2xl">eBanking</p>
+                  <p className="font-bold text-2xl">Contact and Services</p>
 
-              
-            </div>  
+                  <p className="font-bold text-2xl">Mortgages</p>
+                  <p className="font-bold text-2xl">Locations and branches</p>
+
+                  <p className="font-bold text-2xl">3rd pillar</p>
+                  <p className="font-bold text-2xl">Offers for families</p>
+
+                  <p className="font-bold text-2xl">Perks</p>
+                
+              </div>
+  </div>
+</div>
+
 
 
           
